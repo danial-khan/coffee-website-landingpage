de875155f77b2b1a25c5d4ebcf600dceb3390ecc
faizan <faizan@gmail.com>
2025-04-17 20:42:26 +0500

    Time: 01:14:11 done

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ menuOpenButton.addEventListener("click", () => {
 });
 //Close menu when close button is clicked
 menuCloseButton.addEventListener("click", ()=> menuOpenButton.click())
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // Responsive Breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+
+});
